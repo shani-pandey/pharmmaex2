5b7ea521ffb8039f0e0ccecb3cdddbc5eb334e40
Ankur Badola <ankybd811@gmail.com>
2025-07-03 23:46:22 +0530

    minor fix below footer

--- pages/_app.js
+++ pages/_app.js
@@ -109,7 +109,7 @@ export default function App({ Component, pageProps }) {
           }}
         />
       </Head>
-       <Component {...pageProps} />;
+       <Component {...pageProps} />
   </>
   )
  
