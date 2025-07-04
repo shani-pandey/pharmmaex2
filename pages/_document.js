ce247c5e200d97942663088a47405262aad7c999
shani-techv1 <shanipandeytech@gmail.com>
2025-07-04 11:50:33 +0530

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+9oQjw7E/g1zc1U+AMvyTG2xXr5sj" crossorigin="anonymous"></script>
-
+  
       <body>
         <Main />
         <NextScript />
