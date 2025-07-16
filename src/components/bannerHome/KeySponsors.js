bce2dc373852bed92271d7ee2a39339ee91d0123
aartiipcollege <aarti.ggupta2004@gmail.com>
2025-07-16 20:06:41 +0530

    Content Update

--- src/components/bannerHome/KeySponsors.js
+++ src/components/bannerHome/KeySponsors.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const sponsors = [
   [
-    { label: 'Industry Leaders', img: '/assests/img/sp1.svg' },
-    { label: 'Top Rankers', img: '/assests/img/sp2.svg' },
-    { label: 'Pharma Player', img: '/assests/img/sp3.svg' },
-    { label: 'Pharma Champion', img: '/assests/img/sp4.svg' },
+    { label: 'Industry Leaders', img: '/assests/img/Sponsor Logo/Sponsor Logo/Industry-Leader.png' },
+    { label: 'Top Rankers', img: '/assests/img/Sponsor Logo/Sponsor Logo/Top-Ranker.png' },
+    { label: 'Pharma Player', img: '/assests/img/Sponsor Logo/Sponsor Logo/Pharma-Player.png' },
+    { label: 'Pharma Champion', img: '/assests/img/Sponsor Logo/Sponsor Logo/Pharma-Champion.png' },
+    { label: 'Supported Partners', img: '/assests/img/Sponsor Logo/Sponsor Logo/Supported-Partner.png' },
   ],
   [
-    { label: 'Supported Partners', img: '/assests/img/sp5.svg' },
-    { label: 'Supported Partners', img: '/assests/img/sp6.svg' },
-    { label: 'Supported Partners', img: '/assests/img/sp7.svg' },
+    { label: 'Endorsed By', img: '/assests/img/Sponsor Logo/Sponsor Logo/Endorsed By.png' },
+    { label: 'Affilated Partner', img: '/assests/img/Sponsor Logo/Sponsor Logo/Affilate-Partner.jpg' },
+   { label: 'Association-01', img: '/assests/img/Sponsor Logo/Sponsor Logo/Association-01.jpg' },
+     { label: 'Association-02', img: '/assests/img/Sponsor Logo/Sponsor Logo/Association-02.jpg' },
+      { label: 'Association-03', img: '/assests/img/Sponsor Logo/Sponsor Logo/Association-03.jpg' },
   ]
 ];
 
